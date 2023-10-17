f9cd20ecd8e5d9c388ebf31a64f2c6cb1aed2ca5
sjmitch-git <sjmitch20@outlook.com>
2023-10-17 16:16:21 +0100

    fix: functions modified

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -2,7 +2,7 @@
 
 // Add rest parameters here!
 export function add(...nums) {
-  return nums.reduce((acc, val) => acc + val, 0);
+  return nums.reduce((total, val) => total + val, 0);
 }
 
 add(1, 2, 3, 4, 5);
